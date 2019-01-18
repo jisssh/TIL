7dd38abed258a95bd76dfd86c4c7af9ae46a2b9d
jisssh <tkdgus2683@gmail.com>
2019-01-18 17:57:07 +0900

    ss3 | get data | ~190118

--- 01_python/get_data/get_lotto.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-URL = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=840'
-
-response = requests.get(URL)
-
-print(response)
--- 새 폴더/dd.py
+++
@@ -1,2 +0,0 @@
-'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json'
-key = 32f5ec60c73a41954bba1da0470dc0cb
