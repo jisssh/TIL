6c2606f7db3a587697a9c8418fc9832093ce4299
jisssh <tkdgus2683@gmail.com>
2019-04-09 17:48:07 +0900

    ss3 | Django Master Jiss | ~190409

--- 07_Django/review_prj/practice/urls.py
+++ 07_Django/review_prj/practice/urls.py
@@ -4,6 +4,11 @@ from . import views
 app_name = 'board_ad'
 
 urlpatterns = [
+    path('new/', views.posting_create, name='posting_create'),
     path('', views.posting_list, name='posting_list'),
-    path('<int:id>/', views.posting_detail, name='posting_detail'),
+    path('<int:posting_id>/', views.posting_detail, name='posting_detail'),
+    path('<int:posting_id>/update/', views.posting_update, name='posting_update'),
+    path('<int:posting_id>/delete/', views.posting_delete, name='posting_delete'),
+    path('<int:posting_id>/comments/create/', views.comment_create, name='comment_create'),
+    path('<int:posting_id>/comments/<int:comment_id>/delete/', views.comment_delete, name='comment_delete'),
 ]
--- 07_Django/review_prj/practice/views.py
+++ 07_Django/review_prj/practice/views.py
@@ -1,15 +1,74 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods
 from .models import Posting, Comment
 
 
+# create
+@require_http_methods(['GET', 'POST'])
+def posting_create(request):
+    if request.method == 'POST':
+        posting = Posting()
+        posting.title = request.POST.get('title')
+        posting.content = request.POST.get('content')
+        posting.save()
+        return redirect('board_ad:posting_detail', posting.id)
+    else:
+        return render(request, 'practice/new.html')
+
+
+# Read
+@require_http_methods(['GET'])
 def posting_list(request):
     postings = Posting.objects.all()
     return render(request, 'practice/list.html', {
         'postings': postings,
     })
 
-def posting_detail(request, id):
-    posting = Posting.objects.get(id=id)
+
+@require_http_methods(['GET'])
+def posting_detail(request, posting_id):
+    # posting = Posting.objects.get(id=id)
+    posting = get_object_or_404(Posting, id=posting_id)
+    comments = posting.comment_set.all()
     return render(request, 'practice/detail.html', {
         'posting': posting,
+        'comments': comments,
     })
+
+
+# Update
+def posting_update(request, posting_id):
+    posting = get_object_or_404(Posting, id=posting_id)
+    if request.method == "POST":
+        posting.title = request.POST.get('title')
+        posting.content = request.POST.get('content')
+        posting.save()
+        return redirect('board_ad:posting_detail', posting_id=posting.id)
+    else:
+        return render(request, 'practice/edit.html', {'posting': posting})
+
+
+@require_http_methods(['GET', 'POST'])
+def posting_delete(request, posting_id):
+    posting = get_object_or_404(Posting, id=posting_id)
+    posting.delete()
+    return redirect('board_ad:posting_list')
+
+
+# comment - Create
+@require_http_methods(['POST'])
+def comment_create(request, posting_id):
+    posting = get_object_or_404(Posting, posting_id)
+    comment = Comment()
+    comment.posting = Posting.objects.get(posting_id)
+    comment.content = request.POST.get('comment')
+    comment.save()
+    return redirect('board_ad:posting_detail', posting_id)
+
+
+@require_http_methods(['POST'])
+def comment_delete(request, posting_id, comment_id):
+    posting = get_object_or_404(Posting, id=posting_id)
+    comment = get_object_or_404(Comment, id=comment_id)
+    comment.delete()
+    return redirect('board_ad:posting_detail', posting_id=posting.id)
