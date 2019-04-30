a2a03b85f97ce61a39f325015595018f3a1f11cd
지상현 <jisssh@naver.com>
2019-04-30 15:13:04 +0900

    ss3 | JS_blockingNonblocking | ~190430

--- 08_JS/05_blockingNonblocking/setTimeout.js
+++ 08_JS/05_blockingNonblocking/setTimeout.js
@@ -1,13 +1,11 @@
 function sleep_3s() {
-    setTimeout(() => {console.log('wake up!')}, 3000)
+    console.log('INVOKE');
+    setTimeout(() => {console.log('WAKE UP!')}, 3000)
 }
-
-
-
 
 const logEnd = () => {
     console.log('END')
 };
-console.log('startsleeping');
+console.log('start sleeping');
 sleep_3s();
 console.log('end');
